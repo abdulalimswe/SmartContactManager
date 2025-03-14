92b04aef6845177ee62bbeab8b5f54e16a118ec1
abdulalimswe <abdulalimdhuronto24@gmail.com>
2025-03-15 01:38:23 +0600

    update

--- src/main/java/com/utin/scm/controllers/PageController.java
+++ src/main/java/com/utin/scm/controllers/PageController.java
@@ -2,7 +2,9 @@ package com.utin.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
@@ -16,5 +18,32 @@ public class PageController {
     public String Home(Model model) {
         model.addAttribute("name", "Welcome to the SCM");
         return "home";
+    }
+
+    @RequestMapping("/about")
+    public String About(Model model) {
+        model.addAttribute("name", "About Page");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String servicesPage(Model model) {
+        model.addAttribute("name", "Services Page");
+        return "services";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return new String("register");
     }
 }
