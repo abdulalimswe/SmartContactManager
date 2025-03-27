b510e54083e5537e2575ac66246148f9cbeaf4ec
abdulalimswe <abdulalimdhuronto24@gmail.com>
2025-03-27 23:11:42 +0600

    registerDB

--- src/main/java/com/utin/scm/controllers/PageController.java
+++ src/main/java/com/utin/scm/controllers/PageController.java
@@ -1,20 +1,25 @@
 package com.utin.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.utin.scm.entities.User;
+import com.utin.scm.forms.UserForm;
+import com.utin.scm.services.UserService;
+
 
 @Controller
 public class PageController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/")
-    public String Index(Model model) {
-        model.addAttribute("name", "Welcome to the SCM Page Index");
-        return "index";
-    }
-    @RequestMapping("/home")
     public String Home(Model model) {
         model.addAttribute("name", "Welcome to the SCM");
         return "home";
@@ -43,7 +48,28 @@ public class PageController {
     }
 
     @GetMapping("/register")
-    public String register() {
-        return new String("register");
+    public String register(Model model) {
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
+        return "register";
     }
+
+    @RequestMapping(value = "/do-register", method=RequestMethod.POST)
+    public String processRegister(@ModelAttribute UserForm userForm) {
+        
+        User user = User.builder()
+            .name(userForm.getName())
+            .email(userForm.getEmail())
+            .password(userForm.getPassword())
+            .about(userForm.getAbout())
+            .phoneNumber(userForm.getPhoneNumber())
+            .build();
+
+        System.out.println(userForm);
+        User savedUser = userService.saveUser(user);
+        System.out.println(savedUser);
+
+        return "redirect:/login";
+    }
+    
 }
